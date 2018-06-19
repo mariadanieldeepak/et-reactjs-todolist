3c469848ddd81e6a90f2b51e58de459319e1f5f6
Maria Daniel Deepak <mariadanieldeepak@gmail.com>
2018-06-19 15:13:02 +0530

    Include a Todolist component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Todolist extends Component {
+    // Every Component must render()
+    render() {
+        // Render() must return something or should return `null`;
+        return (
+            <div className="Todolist-welcome">Hello! Check out the tasks you've added.</div>
+        );
+    }
+}
+
 class App extends Component {
   render() {
     return (
@@ -11,7 +21,7 @@ class App extends Component {
           <h1 className="App-title">A Todolist app using ReactJS</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Todolist/>
         </p>
       </div>
     );
