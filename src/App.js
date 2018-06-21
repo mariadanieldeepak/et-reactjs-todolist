25e2db9bbfb2b8cfccc1e1f393dae3e52b1775d8
Maria Daniel Deepak <mariadanieldeepak@gmail.com>
2018-06-21 13:32:56 +0530

    Fix styles to make app more visually appealing

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import './App.css';
 import Todolist from './Components/Todolist';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">A Todolist app using ReactJS</h1>
-        </header>
-        <div className="App-intro">
-          <Todolist/>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">A Todolist app using ReactJS</h1>
+                </header>
+                <div className="App-intro">
+                    <Todolist/>
+                    <div className="App-component-types">
+                        Controlled vs Un-controlled components
+                        <br />
+                        Hello World.
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
